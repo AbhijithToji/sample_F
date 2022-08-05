65fcb4a7157da42a35bec5439ccdc4ffe74ee0b3
Abhijith Olikkara <abhijitholikkara@gmail.com>
2022-08-05 20:39:36 +0530

    Commit

---
+++ lib/Validation.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: FormValidationExample(),
+  ));
+}
+
+class FormValidationExample extends StatelessWidget with InputValidationMixin {
+  final formGlobalKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Registration Page'),
+        ),
+
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Form(
+            key: formGlobalKey,
+            child: Column(
+              children: [
+                const SizedBox(height: 100),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Email"),
+                  validator: (email) {
+                    if (isEmailValid(email!))
+                      return null;
+                    else
+                      return 'Enter a valid email address';
+                  },
+                ),
+                const SizedBox(height: 24),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                  ),
+                  maxLength: 8,
+                  obscureText: true,
+                  validator: (password) {
+                    if (isPasswordValid(password!))
+                      return null;
+                    else
+                      return 'Enter a valid password';
+                  },
+                ),
+                const SizedBox(height: 50),
+                ElevatedButton(
+                    onPressed: () {
+                      if (formGlobalKey.currentState!.validate()) {
+                        formGlobalKey.currentState!.save();
+                        onPressed: () {
+
+
+                        };
+                      }
+                    },
+                    child: Text("Submit"))
+
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+
+mixin InputValidationMixin {
+  bool isPasswordValid(String password) => password.length == 4;
+
+  bool isEmailValid(String email) {
+    String pattern =
+        r'^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = RegExp(pattern);
+    return regex.hasMatch(email);
+  }
+}
